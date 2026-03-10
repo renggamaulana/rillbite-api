c857f60702771197316d68bb2766f56ffbeebdf0
renggamaulana <renggaa21@gmail.com>
2026-03-11 00:31:26 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->validateCsrfTokens(except: [
-            'api/*',
-        ]);
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdmin::class,
         ]);

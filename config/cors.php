813119c001c5d3ce4f8fc044d20175eea7489771
renggamaulana <renggaa21@gmail.com>
2026-03-11 00:54:27 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -3,13 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://127.0.0.1:3000',
-        'https://rillbite.vercel.app',
-        'https://rillbite.com',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
